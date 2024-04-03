d99959fb8bd9986c6c923bcbbf40dd4c1a2e5442
Kashawn-Brown <kashawnbrown@hotmail.com>
2024-04-02 20:53:59 -0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require("body-parser");
 const cors = require('cors');
+const path = require("path");
 
 // Getting access to .env file
 require('dotenv').config({ path: '../.env' });
