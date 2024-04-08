85b848205fa3d968fd335ed9fbf02f5935719240
Kashawn-Brown <kashawnbrown@hotmail.com>
2024-04-08 01:06:44 -0400

    Deployment I
    Code for the deployed project @ http://15.157.118.12/

--- frontend/src/api/axiosConfig.js
+++ frontend/src/api/axiosConfig.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: 'http://localhost:5000/',
+    // baseURL: 'http://localhost:5000/',
+    baseURL: 'http://15.157.118.12:5000/',
     headers: {"ngrok-skip-browsing-warning": "true"}
 })
 
